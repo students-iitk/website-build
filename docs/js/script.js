20e811ea1ef3f127a03e7fa7643b5c594ef4f4e1
mdrahbar21 <mdrahbar21@iitk.ac.in>
2022-11-02 22:05:23 +0530

    mentorship updated

---
+++ docs/js/script.js
@@ -0,0 +1,25 @@
+/* 
+==============================================
+* Green Paper - v1.0
+==============================================
+
+* Demo: https://demo.uicard.io/green-paper-pro/
+* Free Version: https://uicard.io/products/green-paper
+* Premium Version: https://uicard.io/products/green-paper-pro
+
+==============================================
+* Coded By : UICardio https://uicard.io
+==============================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* Copyright 2018 UICardio
+*/
+
+$(document).ready(function(){
+  
+  //Activating tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+
+});
